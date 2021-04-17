43171903000ea2b467ae0ae58824cbe442539ef1
Mohsen Hatami <mohsen_hatami@ymail.com>
2021-04-17 19:12:43 +0430

    New widget added
    DrawPath widget added.

---
+++ toolsLibrary/src/main/java/com/bvtech/toolslibrary/widget/DrawerPathView.kt
@@ -0,0 +1,106 @@
+package com.bagi.drawpath
+
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.bagi.drawpath.DrawerPathView.UiUtils.px
+import com.bvtech.toolslibrary.R
+import kotlin.math.max
+
+class DrawerPathView : View {
+    private var path: Path? = null
+    private var paint: Paint? = null
+    private var length = 0f
+    private var roundCorner = 28
+    private var paddingRectangleFromEdge = 5f
+    private var animatorDuration = 6000
+    private var colorPath = 0xff000000.toInt()
+
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        initAttrs(attrs)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        initAttrs(attrs)
+    }
+
+    private fun initAttrs(attrs: AttributeSet?) {
+        val arr = context.obtainStyledAttributes(attrs, R.styleable.WidgetAttributes)
+        arr.let {
+            paddingRectangleFromEdge =
+                it.getFloat(R.styleable.WidgetAttributes_paddingRectangleFromEdge, 5f)
+            animatorDuration = it.getInt(R.styleable.WidgetAttributes_animatorDurationSec, 6)
+            roundCorner = it.getInt(R.styleable.WidgetAttributes_roundCornerInPx, 28)
+            colorPath = it.getColor(R.styleable.WidgetAttributes_colorPath, 0xff000000.toInt())
+        }
+        arr.recycle()
+    }
+
+    fun init(widthRec: Int, heightRec: Int) {
+        setPaint()
+        setPathToDraw(widthRec, heightRec)
+        setAnimator()
+    }
+
+    private fun setAnimator() {
+        // Measure the path
+        val measure = PathMeasure(path, false)
+        length = measure.length
+        val animator = ObjectAnimator.ofFloat(this@DrawerPathView, "phase", 1.0f, 0.0f)
+        animator.duration = animatorDuration.times(1000).toLong()
+        animator.start()
+    }
+
+    //is called by animator object "propertyName" parameter
+    fun setPhase(phase: Float) {
+        paint?.pathEffect = createPathEffect(length, phase, 0.0f)
+        invalidate()
+    }
+
+    private fun createPathEffect(pathLength: Float, phase: Float, offset: Float) =
+        DashPathEffect(
+            floatArrayOf(pathLength, pathLength),
+            max(phase * pathLength, offset)
+        )
+
+
+    private fun setPathToDraw(widthRec: Int, heightRec: Int) {
+        path = Path()
+        val rectF = RectF(
+            paddingRectangleFromEdge,
+            paddingRectangleFromEdge,
+            (widthRec - paddingRectangleFromEdge),
+            (heightRec - paddingRectangleFromEdge)
+        )
+        path?.addRoundRect(rectF, roundCorner.toFloat().px.toFloat(), roundCorner.toFloat().px.toFloat(), Path.Direction.CW)
+    }
+
+    private fun setPaint() {
+        paint = Paint()
+        paint?.color = colorPath
+        paint?.strokeWidth = 5f
+        paint?.style = Paint.Style.STROKE
+        paint?.isAntiAlias = true
+    }
+
+    public override fun onDraw(c: Canvas) {
+        super.onDraw(c)
+        path?.let { pathT ->
+            paint?.let { paintT -> c.drawPath(pathT, paintT) }
+        }
+    }
+
+    object UiUtils {
+        val Float.dp: Int get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+        val Float.px: Int get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+        fun getWidthDisplayScreenSize() = Resources.getSystem().displayMetrics.widthPixels
+    }
+}
